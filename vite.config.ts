3d8966ae7e2bb7bb0c79bc5fcdda2a596aac9f5d
francoloncarica <francoloncarica@hotmail.com>
2025-05-12 20:59:12 -0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: 'docs',  // ← salida en carpeta docs
+    assetsDir: 'assets'
+  },
   plugins: [
     react(),
     mode === 'development' &&
