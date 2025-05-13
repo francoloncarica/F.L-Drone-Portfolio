8d0d5bfb3a501b25437884c57709f089abacf74b
francoloncarica <francoloncarica@hotmail.com>
2025-05-12 21:11:33 -0300

    afasf

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './', // ✅ ESTA LÍNEA ES CLAVE PARA GITHUB PAGES
+  base: '/black-aerial-visions/',
   server: {
     host: "::",
     port: 8080,
