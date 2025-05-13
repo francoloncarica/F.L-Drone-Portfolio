778f7102052c3b7c90520c56cafcc498ab7a97bc
Franco Loncarica <36172650+francoloncarica@users.noreply.github.com>
2025-05-13 10:35:50 -0300

    cambio titulo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/black-aerial-visions/',
+  base: '/F.L-Drone-Portfolio/',
   server: {
     host: "::",
     port: 8080,
